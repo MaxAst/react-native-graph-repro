d58594c738ec260d2af99182da87a552c94b2c77
Max Ast <maxast31@gmail.com>
2024-10-18 07:12:45 +0200

    add animated bug repro

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { StyleSheet } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { GraphPage } from './GraphPage'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <GestureHandlerRootView style={styles.container}>
+      <SafeAreaProvider>
+        <GraphPage />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+})
